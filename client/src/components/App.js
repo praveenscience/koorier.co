67bf36272bc2a19b3da54a312eb16cab5f5007e4
Praveen Kumar <praveenscience@gmail.com>
2021-11-21 07:40:42 +0000

    Fixed the header issues and added static pages.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import KooNavBar from "./NavBar";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App NavBarPadding">
       <KooNavBar />
+      <Switch>
+        <Route path="/about">About Page</Route>
+        <Route path="/faqs">FAQs Page</Route>
+        <Route path="/work">How it Works Page</Route>
+        <Route path="/" exact={true}>
+          Home Page
+        </Route>
+      </Switch>
     </div>
   );
 };
