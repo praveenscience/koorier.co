1899f0daa50b5b07cff5f4857b48a2cd798df62a
Praveen Kumar <praveenscience@gmail.com>
2021-11-21 07:18:46 +0000

    Added Header.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,53 @@
 import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
 const App = () => {
-  return <div className="App">Koorier</div>;
+  return (
+    <div className="App">
+      <Navbar color="dark" dark expand="md" fixed="top">
+        <NavbarBrand href="/">
+          <img
+            src="https://i.imgur.com/wzqOe1S.png"
+            alt="Koorier"
+            width="30"
+            height="30"
+          />
+        </NavbarBrand>
+        <NavbarToggler onClick={function noRefCheck() {}} />
+        <Collapse navbar>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/About">About</NavLink>
+            </NavItem>
+            <UncontrolledDropdown inNavbar nav>
+              <DropdownToggle caret nav>
+                FAQs
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>All Questions</DropdownItem>
+                <DropdownItem>How it Works?</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
 };
 
 export default App;
