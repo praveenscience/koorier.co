39b9c532cccb55217953b99ddea1d635276c0c07
Praveen Kumar <praveenscience@gmail.com>
2021-11-21 07:28:41 +0000

    Fixed the NavBar with React Router & Containers.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import {
   Navbar,
   NavbarBrand,
@@ -6,16 +7,14 @@ import {
   Collapse,
   Nav,
   NavItem,
-  NavLink,
   UncontrolledDropdown,
   DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+  DropdownMenu
 } from "reactstrap";
 
 const KooNavBar = () => {
   return (
-    <Navbar color="dark" dark expand="md" fixed="top">
+    <Navbar color="dark" dark expand="md" fixed="top" container>
       <NavbarBrand href="/">
         <img
           src="https://i.imgur.com/wzqOe1S.png"
@@ -28,18 +27,26 @@ const KooNavBar = () => {
       <Collapse navbar>
         <Nav className="ms-auto" navbar>
           <NavItem>
-            <NavLink href="/">Home</NavLink>
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/About">About</NavLink>
+            <NavLink to="/About" className="nav-link">
+              About
+            </NavLink>
           </NavItem>
           <UncontrolledDropdown inNavbar nav>
             <DropdownToggle caret nav>
               FAQs
             </DropdownToggle>
             <DropdownMenu right>
-              <DropdownItem>All Questions</DropdownItem>
-              <DropdownItem>How it Works?</DropdownItem>
+              <NavLink to="/faqs" className="dropdown-item">
+                All Questions
+              </NavLink>
+              <NavLink to="/work" className="dropdown-item">
+                How it Works?
+              </NavLink>
             </DropdownMenu>
           </UncontrolledDropdown>
         </Nav>
