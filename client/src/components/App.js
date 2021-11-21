82640dbf48d98d428f8bb1d44079f99af099d25d
Praveen Kumar <praveenscience@gmail.com>
2021-11-21 07:52:59 +0000

    Converted the App into Class Component.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import KooNavBar from "./NavBar";
 
-const App = () => {
-  return (
-    <div className="App NavBarPadding">
-      <KooNavBar />
-      <Switch>
-        <Route path="/about">About Page</Route>
-        <Route path="/faqs">FAQs Page</Route>
-        <Route path="/work">How it Works Page</Route>
-        <Route path="/" exact={true}>
-          Home Page
-        </Route>
-      </Switch>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App NavBarPadding">
+        <KooNavBar />
+        <Switch>
+          <Route path="/about">About Page</Route>
+          <Route path="/faqs">FAQs Page</Route>
+          <Route path="/work">How it Works Page</Route>
+          <Route path="/" exact={true}>
+            Home Page
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default App;
