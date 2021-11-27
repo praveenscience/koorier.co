e804f5cdd0aa6d6de750c38d8433477cd4954617
Praveen Kumar <praveenscience@gmail.com>
2021-11-27 06:58:05 +0000

    Updated the data structure for user.

--- client/src/helpers/AuthHelper.js
+++ client/src/helpers/AuthHelper.js
@@ -1,12 +1,20 @@
 const Users = {
-  Praveen: "Praveen",
-  Koorier: "Koorier"
+  Praveen: {
+    Password: "Praveen",
+    Fullname: "Praveen Kumar",
+    Email: "praveen@koorier.com"
+  },
+  Koorier: {
+    Password: "Koorier",
+    Fullname: "Koorier Kumar",
+    Email: "koorier@koorier.com"
+  }
 };
 export const LoginUser = (Username, Password) => {
   if (typeof Users[Username] === "undefined") {
     // User doesn't exist.
   } else {
-    if (Users[Username] === Password) {
+    if (Users[Username].Password === Password) {
       // Correct User & Password
     } else {
       // Correct User but Wrong Password
