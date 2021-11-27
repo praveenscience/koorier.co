f19936c21c9d47b45ac2fb8a10d7622c78202327
Praveen Kumar <praveenscience@gmail.com>
2021-11-27 08:50:39 +0000

    Created the skeleton for API route.

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,7 @@
 const app = require("express").Router();
-const api = require("./api");
 
 app.get("/", (req, res) => {
-  res.json("API Server Running!");
+  res.json("API Route!");
 });
-app.use("/api", api);
 
 module.exports = app;
