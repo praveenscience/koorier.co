70ebae720b69c86ea96c50a560d45bd43a6dab74
Praveen Kumar <praveenscience@gmail.com>
2021-11-27 08:55:13 +0000

    Skeleton for session handling.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -3,5 +3,14 @@ const app = require("express").Router();
 app.get("/", (req, res) => {
   res.json("Tell if it's auth'd");
 });
+app.post("/", (req, res) => {
+  res.json("Call the login function");
+});
+app.put("/", (req, res) => {
+  res.json("Call the register function");
+});
+app.delete("/", (req, res) => {
+  res.json("Call the logout function");
+});
 
 module.exports = app;
