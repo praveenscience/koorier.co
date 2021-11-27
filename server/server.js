9388f01e906dc2b5ade1d21fe445a90cb8939e7b
Praveen Kumar <praveenscience@gmail.com>
2021-11-27 08:47:52 +0000

    Added middleware for body parsing & cors.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
+// Middleware
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.json("API Server Running!");
 });
