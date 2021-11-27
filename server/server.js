5c60c38edc242c69c8e7d7b2a160d9db73fbfd82
Praveen Kumar <praveenscience@gmail.com>
2021-11-27 08:53:30 +0000

    Added Sessions Support.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const session = require("express-session");
 const app = express();
 const port = 5000;
 
@@ -12,6 +13,15 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const sess = {
+  secret: "k00ri3r",
+  cookie: {}
+};
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sess.cookie.secure = true; // serve secure cookies
+}
+app.use(session(sess));
 
 app.use("/", root);
 
