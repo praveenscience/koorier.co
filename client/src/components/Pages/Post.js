147e9aab653d27e39eda0a37653f713efa31f914
Praveen Kumar <praveenscience@gmail.com>
2021-11-27 08:36:54 +0000

    Styled and Created the Post page.

--- client/src/components/Pages/Post.js
+++ client/src/components/Pages/Post.js
@@ -3,39 +3,86 @@ import { useState } from "react";
 const Post = () => {
   const [Selected, setSelected] = useState(null);
   return (
-    <div className="post">
+    <div className="Post">
+      <h1>Post a Request</h1>
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h1>Post a Request</h1>
             <form>
-              <div className="styled-radio">
-                <label>
-                  <input
-                    type="radio"
-                    name="type"
-                    value="delivery"
-                    checked={Selected === "delivery"}
-                    onChange={() => {
-                      setSelected("delivery");
-                    }}
-                  />
-                  <span className="radio-contents">Delivery Requests</span>
-                </label>
-              </div>
-              <div className="styled-radio">
-                <label>
-                  <input
-                    type="radio"
-                    name="type"
-                    value="courier"
-                    checked={Selected === "courier"}
-                    onChange={() => {
-                      setSelected("courier");
-                    }}
-                  />
-                  <span className="radio-contents">Courier Travels</span>
-                </label>
+              <h2>Tell us what you are posting</h2>
+              <div className="d-flex">
+                <div className="styled-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="type"
+                      value="delivery"
+                      checked={Selected === "delivery"}
+                      onChange={() => {
+                        setSelected("delivery");
+                      }}
+                    />
+                    <span className="radio-contents">
+                      <span className="radio-contents-box">
+                        <i
+                          className={`check-icon fa fa${
+                            Selected === "delivery" ? "-check" : ""
+                          }-circle`}
+                        ></i>
+                        <i className="fa fa-archive"></i>
+                        <strong>Delivery Requests</strong>
+                      </span>
+                      <span className="d-block pt-3">
+                        <span className="d-block">
+                          <strong>Delivery Requests</strong>
+                        </span>
+                        <span className="d-block">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Suscipit quia quo, laborum doloremque maiores
+                          dolorem voluptatem facilis fuga repellendus. Incidunt
+                          sit officia quisquam sequi necessitatibus dolorem
+                          numquam perferendis reiciendis unde.
+                        </span>
+                      </span>
+                    </span>
+                  </label>
+                </div>
+                <div className="styled-radio">
+                  <label>
+                    <input
+                      type="radio"
+                      name="type"
+                      value="courier"
+                      checked={Selected === "courier"}
+                      onChange={() => {
+                        setSelected("courier");
+                      }}
+                    />
+                    <span className="radio-contents">
+                      <span className="radio-contents-box">
+                        <i
+                          className={`check-icon fa fa${
+                            Selected === "courier" ? "-check" : ""
+                          }-circle`}
+                        ></i>
+                        <i className="fa fa-certificate"></i>
+                        <strong>Courier Travels</strong>
+                      </span>
+                      <span className="d-block pt-3">
+                        <span className="d-block">
+                          <strong>Courier Travels</strong>
+                        </span>
+                        <span className="d-block">
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Suscipit quia quo, laborum doloremque maiores
+                          dolorem voluptatem facilis fuga repellendus. Incidunt
+                          sit officia quisquam sequi necessitatibus dolorem
+                          numquam perferendis reiciendis unde.
+                        </span>
+                      </span>
+                    </span>
+                  </label>
+                </div>
               </div>
               {Selected && (
                 <button
