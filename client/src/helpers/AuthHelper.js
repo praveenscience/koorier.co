a5418e2a15c186e1e789655867e38bf9b49157ea
Praveen Kumar <praveenscience@gmail.com>
2021-11-28 07:18:20 +0000

    Removed the client side functionality for Auth.

--- client/src/helpers/AuthHelper.js
+++ client/src/helpers/AuthHelper.js
@@ -1,37 +1,5 @@
-export const LoginUser = (Username, Password) => {
-  if (typeof Users[Username] === "undefined") {
-    return {
-      Error: true,
-      Message: "User doesn't exist."
-    };
-  } else {
-    if (Users[Username].Password === Password) {
-      return {
-        Error: false,
-        Message: { Username, ...Users[Username] }
-      };
-    } else {
-      return {
-        Error: true,
-        Message: "Incorrect Username or Password."
-      };
-    }
-  }
-};
+export const LoginUser = (Username, Password) => {};
 
-export const RegisterUser = (Username, Password, Fullname, Email) => {
-  if (typeof Users[Username] === "undefined") {
-    // User doesn't exist.
-    Users[Username] = {
-      Password,
-      Fullname,
-      Email
-    };
-  } else {
-    // User is already there!
-  }
-};
+export const RegisterUser = (Username, Password, Fullname, Email) => {};
 
-export const LogoutUser = () => {
-  return true;
-};
+export const LogoutUser = () => {};
