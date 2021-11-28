8a1c1927b21b6117f6f4d2a54e1a491a9c66d6fd
Praveen Kumar <praveenscience@gmail.com>
2021-11-28 07:21:40 +0000

    Implemented Register User service.

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -2,5 +2,8 @@ import Axios from "axios";
 
 export const GetCurrentUser = () => Axios.get("/api/auth");
 
-export const LoginUserService = (Username, Password) =>
+export const LoginUser = (Username, Password) =>
   Axios.post("/api/auth", { Username, Password });
+
+export const RegisterUser = (Username, Password, Fullname, Email) =>
+  Axios.put("/api/auth", { Username, Password, Fullname, Email });
