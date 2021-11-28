3d0dba9db01400313b0aa25af5d73dda157c3a90
Praveen Kumar <praveenscience@gmail.com>
2021-11-28 07:17:40 +0000

    Removed the dummy Users data.

--- client/src/helpers/AuthHelper.js
+++ client/src/helpers/AuthHelper.js
@@ -1,15 +1,3 @@
-const Users = {
-  Praveen: {
-    Password: "Praveen",
-    Fullname: "Praveen Kumar",
-    Email: "praveen@koorier.com"
-  },
-  Koorier: {
-    Password: "Koorier",
-    Fullname: "Koorier Kumar",
-    Email: "koorier@koorier.com"
-  }
-};
 export const LoginUser = (Username, Password) => {
   if (typeof Users[Username] === "undefined") {
     return {
