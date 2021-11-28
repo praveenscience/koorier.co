27559ea8bc608cde18e2ce604422523e58b0e53f
Praveen Kumar <praveenscience@gmail.com>
2021-11-28 07:20:40 +0000

    Implemented getting of current logged in user.

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -1,4 +1,6 @@
 import Axios from "axios";
 
+export const GetCurrentUser = () => Axios.get("/api/auth");
+
 export const LoginUserService = (Username, Password) =>
   Axios.post("/api/auth", { Username, Password });
