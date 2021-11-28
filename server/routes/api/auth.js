c6c32c50aa13d276362f8c62e99722fb252f88f7
Praveen Kumar <praveenscience@gmail.com>
2021-11-28 07:08:11 +0000

    Implemented login on server side.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,4 +1,5 @@
 const app = require("express").Router();
+const Users = require("../../constants/Users.json");
 
 app.get("/", (req, res) => {
   if (req.session.User) {
@@ -12,10 +13,37 @@ app.get("/", (req, res) => {
       Message: "User not logged in."
     });
   }
-  res.json("Tell if it's auth'd");
 });
 app.post("/", (req, res) => {
-  res.json("Call the login function");
+  const { Username, Password } = req.body;
+  if (!Username || !Password) {
+    res.status(400).json({
+      Error: true,
+      Message: "Need both Username and Password!"
+    });
+  } else {
+    if (typeof Users[Username] === "undefined") {
+      res.status(404).json({
+        Error: true,
+        Message: "User not found!"
+      });
+    } else {
+      if (Users[Username].Password === Password) {
+        const User = { Username, ...Users[Username] };
+        delete User.Password;
+        req.session.User = User;
+        res.json({
+          Error: false,
+          Message: req.session.User
+        });
+      } else {
+        res.status(404).json({
+          Error: true,
+          Message: "Username and Password Incorrect!"
+        });
+      }
+    }
+  }
 });
 app.put("/", (req, res) => {
   res.json("Call the register function");
