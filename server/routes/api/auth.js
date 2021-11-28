0629e416a14aa7e695dbc8b34d406a8939c738ad
Praveen Kumar <praveenscience@gmail.com>
2021-11-28 07:02:06 +0000

    Implemented server session info.

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,6 +1,17 @@
 const app = require("express").Router();
 
 app.get("/", (req, res) => {
+  if (req.session.User) {
+    res.json({
+      Error: false,
+      Message: req.session
+    });
+  } else {
+    res.status(403).json({
+      Error: true,
+      Message: "User not logged in."
+    });
+  }
   res.json("Tell if it's auth'd");
 });
 app.post("/", (req, res) => {
