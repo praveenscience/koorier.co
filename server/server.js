cf5971f0de8b61558d9275f2ca0852ce8220ce31
Praveen Kumar <praveenscience@gmail.com>
2021-11-28 06:56:57 +0000

    Fixed express sessions issue.

--- server/server.js
+++ server/server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const sess = {
   secret: "k00ri3r",
-  cookie: {}
+  cookie: {},
+  resave: true,
+  saveUninitialized: true
 };
 if (app.get("env") === "production") {
   app.set("trust proxy", 1); // trust first proxy
